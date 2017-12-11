c5303e3dadee40910cc5bf198811f1a48957f848
chanu <yuyutyanu@yahoo.co.jp>
2017-12-12 04:11:44 +0900

    add page login/top

---
+++ pages/Login/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="full-background">
+    <div class="login-form-wrapper">
+      <div class="login-form">
+        <h1 class="login-form__title">Apo</h1>
+        <input type="text" placeholder="id">
+        <input type="password" placeholder="pass">
+        <nuxt-link to="/">
+          <button>LOGIN</button>
+        </nuxt-link>
+      </div>
+    </div>
+    <nuxt-link to="register">
+      <span class="to-register">新規登録はこちら</span>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+  export default{
+    data () {
+      return {
+        message: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  a{
+    text-decoration: none;
+  }
+
+  .login-form-wrapper {
+    width: 100%;
+    height: 100vh;
+    position: relative;
+  }
+
+  .login-form {
+    width: 50%;
+    margin: 0 auto;
+    text-align: center;
+    position: absolute;
+    top: 45%;
+    left: 50%;
+    transform: translate(-50%, -50%)
+  }
+
+  .login-form__title {
+    font-size: 70px;
+    margin-bottom: 100px;
+    color: #2c3e50;
+    position: relative;
+  }
+
+  .login-form__title:before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    min-width: 50px;
+    max-width: 50px;
+    left: 50%;
+    transform: translate(-50%, 0);
+    height: 5px;
+    background: #2c3e50;
+  }
+
+  .login-form input {
+    width: 100%;
+    padding: 5px 10px;
+    font-size: 20px;
+    margin-bottom: 20px;
+    border-radius: 3px;
+    border: none;
+    outline: none;
+    color: #2c3e50;
+  }
+
+  .login-form button {
+    display: block;
+    margin-left: auto;
+    background: #2c3e50;
+    color: #f8f8f8;
+    padding: 10px;
+    border-radius: 3px;
+    border: none;
+    outline: none;
+  }
+
+  .to-register {
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+    font-size: 14px;
+    color:#2c3e50;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,120 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        apo
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+  <div class="top">
+    <transition name="fade">
+      <div class="thumbnail-wrap" v-if="isVisible" @click="isVisible = !isVisible">
+        <img :src="imgURL" alt="" class="thumbnail">
+      </div>
+    </transition>
+    <div class="users">
+      <div class="user" v-for="user in users" @click="isVisible = !isVisible">
+        <div class="name">{{user.name}}</div>
       </div>
     </div>
-  </section>
+    <footer>
+      <button>Me</button>
+      <button>Online/Offline</button>
+      <button>Sns</button>
+    </footer>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
+  export default{
+    data () {
+      return {
+        imgURL: 'https://avatars0.githubusercontent.com/u/17114906?s=460&v=4',
+        users: [
+          {name: '田中　太郎'},
+          {name: '大阪　太郎'},
+          {name: '北海道　太郎'},
+          {name: '東京　太郎'},
+          {name: '子　太郎'},
+          {name: '中　太郎'},
+          {name: 'たたた　太郎'},
+          {name: 'それはもう　太郎'},
+          {name: '宝くじ　当て太郎'}
+        ],
+        isVisible: false
+      }
+    }
   }
-}
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style scoped>
+  .top {
+    height: 100vh;
+  }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .thumbnail-wrap {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.6);
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .thumbnail-wrap .thumbnail {
+    position: absolute;
+    display: block;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0
+  }
 
-.links {
-  padding-top: 15px;
-}
+  header {
+    width: 100%;
+    padding: 15px;
+  }
+
+  .users {
+    width: 100%;
+    height: 80vh;
+    margin: 0 auto;
+    overflow: auto;
+  }
+
+  .user {
+    display: flex;
+    background: #f8f8f8;
+  }
+
+  .user:nth-child(3n) {
+    background: #FD6964;
+  }
+
+  .user:hover {
+    background: gray;
+  }
+
+  .user .name {
+    font-size: 17px;
+    font-weight: 800;
+    color: #2c3e50;
+    padding: 20px 20px 20px 40px;
+  }
+
+  footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 25px;
+    text-align: center;
+  }
+
+  footer button {
+    background: #2c3e50;
+    color: #f8f8f8;
+    padding: 15px;
+    border: none;
+    margin-left: 40px;
+  }
+
+  footer button:first-child {
+    margin: 0;
+  }
 </style>
